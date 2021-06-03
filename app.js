f8efb005b9aa27112f9cddca9d1d0832d891ef1c
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-06-03 07:42:36 +0500

    changes made

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const flag = document.getElementById("flag"),name = document.getElementById("name");
+const flag = document.getElementById("flag"),
+  name = document.getElementById("name");
 
 fetch("https://restcountries.eu/rest/v2/all")
-.then(res => res.json())
-.then(data => {
-    for (let i=0; i<data.length; i++) {
-        flag.innerHTML += `<h1>${data[i].name}</h1><img src=${data[i].flag} style="width: 20rem">`;
-    }
-})
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+      flag.innerHTML += `<div style="background: #fff; width: 20rem;"><img src=${country.flag} style="width: 20rem; height: 12rem; object-fit: cover"><h1>${country.name}</h1><strong>Population: </strong><span>${country.population}</span><br><strong>Region: </strong><span>${country.region}</span><br><strong>Capital: </strong><span>${country.capital}</span></div>`;
+    });
+  });
