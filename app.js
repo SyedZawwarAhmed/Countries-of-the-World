f90ceff82d1701cab58a974bcb86efb050c66c75
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-06-04 20:05:22 +0500

    changes made

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ fetch("https://restcountries.eu/rest/v2/all")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((country) => {
-      main.innerHTML += `<div class="country"><img class="flag" src=${country.flag}><h1>${country.name}</h1><strong>Population: </strong><span>${country.population}</span><br><strong>Region: </strong><span>${country.region}</span><br><strong>Capital: </strong><span>${country.capital}</span></div>`;
+      main.innerHTML += `<div class="country"><div class="flag-container"><img class="flag" src=${country.flag}></div><div class="country-details"><h2 class="country-name">${country.name}</h2><span><strong>Population: </strong>${country.population}</span><br><span><strong>Region: </strong>${country.region}</span><br><span><strong>Capital: </strong>${country.capital}</span></div></div>`;
     });
   });
